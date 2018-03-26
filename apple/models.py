39c3c65708ca8e9f58f6d092c74d3c8a54822c45
thiru <a0130737@u.nus.edu>
2018-03-27 01:10:54 +0800

    Add API to store tweets and users

--- apple/models.py
+++ apple/models.py
@@ -29,10 +29,14 @@ class TwitterUser(models.Model):
 	properties = JSONField(default=dict)
 
 	def __str__(self):
-		return "Twitter ID :{} screen_name: {}".format(self.twitter_id,self.screen_name)
+		return "Twitter ID: {} screen_name: {}".format(self.twitter_id,self.screen_name)
 
 
-	def create(user_response_dict):
+	def find_or_create(user_response_dict):
+		query = TwitterUser.objects.filter(twitter_id=user_response_dict['id'])
+		if len(query) > 0:
+			return query.first()
+
 		#TODO: Abstract out to a info extractor class
 		user_info = {'twitter_id': user_response_dict['id'],
 					  'screen_name':user_response_dict['screen_name'],
@@ -47,10 +51,10 @@ class TwitterUser(models.Model):
 					 'raw_response':user_response_dict
 					 }
 
-		
-
 		new_user = TwitterUser(**user_info)
-		return new_user.save()
+		new_user.save()
+		return new_user
+		
 
 
 
@@ -62,28 +66,42 @@ class Tweet(models.Model):
 	tweet_id = models.BigIntegerField(db_index=True) # Index to make searches quicker
 	text = models.CharField(max_length=250,default=None, blank=True, null=True)
 	place = models.CharField(max_length=250,default=None, blank=True, null=True)
-	latitude = models.DecimalField(max_digits=9, decimal_places=6)
-	longitude = models.DecimalField(max_digits=9, decimal_places=6)
+	latitude = models.DecimalField(max_digits=9, decimal_places=6,default=None, blank=True, null=True)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6,default=None, blank=True, null=True)
 	retweet_count = models.BigIntegerField(default=None, blank=True, null=True)
-	favourite_count = models.BigIntegerField(default=None, blank=True, null=True)
+	favorite_count = models.BigIntegerField(default=None, blank=True, null=True)
 
 	raw_response = JSONField(default=dict)
 	properties = JSONField(default=dict)
 
-	def create(tweet_dict):
-	#TODO: Abstract out to a info extractor class
+	def __str__(self):
+		return "Tweet ID: {}".format(self.tweet_id)
+
+
+	def find_or_create(tweet_dict):
+		query = Tweet.objects.filter(tweet_id=tweet_dict['id'])
+		if len(query) > 0:
+			return query.first()
+
+		#TODO: Abstract out to a info extractor class
 		tweet_info = {'tweet_id':tweet_dict['id'],
 						'text':tweet_dict['text'],
 						'place':tweet_dict['place'],
 						'retweet_count':tweet_dict['retweet_count'],
-						'favourite_count':tweet_dict['favourite_count'],
+						'favorite_count':tweet_dict['favorite_count'],
 						'raw_response':tweet_dict
 						}
+
 		coordinates = tweet_dict['coordinates']
 		if coordinates:
 			tweet_info['longitude'] = coordinates['coordinates'][0]
 			tweet_info['latitude'] = coordinates['coordinates'][1]
 		
+		user_id = tweet_dict['user']['id']
+		user = TwitterUser.find_or_create(tweet_dict['user'])
+		tweet_info['user'] = user
+
 		new_tweet = Tweet(**tweet_info)
-		return new_tweet.save()
+		new_tweet.save()
+		return new_tweet
 
--- apple/views.py
+++ apple/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from apple.models import Tweet
 
 def index(request):
 	context = {}
@@ -17,3 +18,22 @@ def index(request):
 def customers(request):
 	context = {}
 	return render(request, 'customers.html', context)
+
+
+@csrf_exempt 
+def stream_api_post(request):
+	if request.method == 'POST':
+		tweet_details = json.loads(request.body)
+		tweet = Tweet.find_or_create(tweet_details)
+		response = {'status':'fail'}
+
+		if tweet:
+			response['status'] = 'success'
+			response['tweet_id'] = tweet.tweet_id
+			response['user_id'] = tweet.user.twitter_id
+			response['screen_name'] = tweet.user.screen_name
+		
+		return HttpResponse(json.dumps(response))
+
+	else: #GET
+		return HttpResponse("Invalid operands")
