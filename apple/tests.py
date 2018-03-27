3787ddccca84f5b9b0e1ac29ad620bcb5a51b19b
thiru <a0130737@u.nus.edu>
2018-03-27 19:41:58 +0800

    Fix tests

--- apple/tests.py
+++ apple/tests.py
@@ -9,17 +9,19 @@ from apple.models import *
 ## Test save a model to database
 
 class ModelTestCase(TestCase):
-    def setUp(self):
-    	with open("test_response_data.json") as f:
-    		data = json.load(f)
+	data = None
 
-    def test_save_user(self):
-    	result = TwitterUser.find_or_create(data['user'])
-    	return self.assertEqual(14065548,result.twitter_id)
+	def setUp(self):
+		global data
+		with open("test_response_data.json") as f:
+			data = json.load(f)
 
+	def test_save_user(self):
+		result = TwitterUser.find_or_create(data['user'])
+		return self.assertEqual(14065548,result.twitter_id)
 
-    def test_save_tweet(self):
-    	result = TwitterUser.find_or_create(data)
-    	return self.assertEqual(result,result.tweet_id)
+	def test_save_tweet(self):
+		result = Tweet.find_or_create(data)
+		return self.assertEqual(978273515135819776,result.tweet_id)
 
     	
